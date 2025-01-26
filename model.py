6d6e0323e559b5549d3d5735b6086a7dc162b30f
dylancsu <dylan.dean@csuglobal.edu>
2025-01-26 16:28:30 -0700

    typo

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class Dean:
         self.delegating = input("Explain the different components of the project: ")
         self.prototyping = input("Input the prototype status: ")
         self.integration = input("Input the integration status: ")
-        self.iterative = input("Input information regarding iterative development")
+        self.iterative = input("Input information regarding iterative development: ")
     def __str__(self):
         return f"SCOPE\n{self.scope}\n\nPLAN\n{self.plan}\n\nDELEGATION\n{self.delegating}\n\nPROTOTYPING\n{self.prototyping}\n\nINTEGRATION\n{self.integration}\n\nITERATION\n{self.iterative}"
 
